1183a31bfb1b6e7df365c17ae42c80f3d00dbebd
lenasidyakina <sidyakina.lena@gmail.com>
2025-10-13 20:09:04 +0300

    DP8

--- PPO_f/app_cli/src/test/resources/db_test_data.sql
+++ PPO_f/app_cli/src/test/resources/db_test_data.sql
@@ -42,10 +42,3 @@ INSERT INTO question_tags(question_id, tags_id) VALUES
 (1, 0),
 (1, 3);
 
-DROP TABLE IF EXISTS extended_answer_tags CASCADE;
-
--- Создаём пустую таблицу без внешних ключей, просто чтобы Hibernate не ругался
-CREATE TABLE extended_answer_tags (
-                                      extended_answer_id BIGINT,
-                                      tags_id BIGINT
-);
