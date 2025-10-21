24c357e62edd55ea70d54f859e1dd6083d4b0d49
lenasidyakina <115419273+lenasidyakina@users.noreply.github.com>
2025-10-22 02:52:46 +0300

    Update schema.sql

--- PPO_f/app_cli/src/test/resources/schema.sql
+++ PPO_f/app_cli/src/test/resources/schema.sql
@@ -1,7 +1,7 @@
 create table if not exists customers (
     id bigserial not null,
-    name varchar not null,
-    email varchar not null,
+    name text not null,
+    email text not null,
     primary key (id),
     UNIQUE (email)
-)
+);
